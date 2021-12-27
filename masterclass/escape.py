adcb7880083b9c94bbb5b8e0badd996ca162a356
nev3rwh3re <51082324+nev3rwh3re@users.noreply.github.com>
2021-12-27 09:46:29 -0800

    Add files via upload

---
+++ masterclass/escape.py
@@ -0,0 +1,18 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('the pet shop owner said "no, no, \'e\'s resting".')
+
+print("the pet shop owner said \"no, no 'e's resting.\".")
+
+print("""the pet shop owner said "no, no, 'e's resting".""")
+
+anotherSplitString = """This string has been
+split over \
+several \
+lines"""
+
+print(anotherSplitString)

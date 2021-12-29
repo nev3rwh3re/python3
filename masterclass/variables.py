d19de98ee82c35547f95a0bfc11409984539ceac
nev3rwh3re <51082324+nev3rwh3re@users.noreply.github.com>
2021-12-28 20:37:48 -0800

    Add files via upload

---
+++ masterclass/variables.py
@@ -0,0 +1,16 @@
+
+age = 742
+print(age)
+name = "Matt"
+
+print(type(age))
+
+age2 = "2 years"
+
+print(age2)
+print(type(age2))
+
+print(str(age) + age2)
+
+print(name + " is " + str(age) + " years old")
+

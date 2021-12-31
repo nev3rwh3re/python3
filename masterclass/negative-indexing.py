d32daabf739f262fdf0d9b9267554f447020cc4b
nev3rwh3re <51082324+nev3rwh3re@users.noreply.github.com>
2021-12-31 10:22:26 -0800

    added
    added

---
+++ masterclass/negative-indexing.py
@@ -0,0 +1,39 @@
+#         01234567890123
+parrot = "Norwegian Blue"
+print(parrot)
+print()
+print(parrot[3])
+
+# mini challenge
+
+print(parrot)
+print(parrot[3])
+print(parrot[4])
+print(parrot[9]) # could also use print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+#1204
+print()
+print()
+print(parrot[-1]) # end of the string
+print(parrot[-14])
+
+print(parrot)
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5]) # could also use print()
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+
+print(parrot)
+print(parrot[3-14])
+print(parrot[4-14])
+print(parrot[9-14]) # could also use print()
+print(parrot[3-14])
+print(parrot[6-14])
+print(parrot[8-14])
+
---
+++ masterclass/string-data-type.py
@@ -0,0 +1,17 @@
+#         01234567890123
+parrot = "Norwegian Blue"
+print(parrot)
+print()
+print(parrot[3])
+
+# mini challenge
+
+print(parrot)
+print(parrot[3])
+print(parrot[4])
+print(parrot[9]) # could also use print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+#313

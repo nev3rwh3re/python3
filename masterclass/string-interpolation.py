d54ca2e4d44ef3463772a700c83cb9b2e621f5c5
nev3rwh3re <51082324+nev3rwh3re@users.noreply.github.com>
2022-01-11 19:07:37 -0800

    added
    add

---
+++ masterclass/string-interpolation.py
@@ -0,0 +1,16 @@
+# Python 2 only - similar to C
+
+print()
+
+age=24
+print("My age is %d years" % age)
+
+major = "years"
+minor = "months"
+print ("My age is %d %s, %d %s" % (age, major, 6, minor))
+print("Pi is about %f" % (22 /7))
+print("Pi is about %60.50f" % (22 /7))
+print()
+
+# string formating operations in python docs
+
